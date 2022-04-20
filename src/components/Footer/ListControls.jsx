54972149310edf7279e2d1485a4525f76859ebfd
rtruc <ubno@me.com>
2022-04-20 19:45:40 -0400

    Added so bad homemade sort Icons. Might not be an improvement...

--- src/components/Footer/ListControls.jsx
+++ src/components/Footer/ListControls.jsx
@@ -2,8 +2,10 @@ import { useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { addTask, sortTitlesAscending, sortTitlesDescending, sortDatesAscending, sortDatesDescending } from "../../redux/actions/actions";
-import {NavItem} from "../NavBar/NavItem"
-import {NavButton} from "../NavBar/NavButton"
+import { NavItem } from "../NavBar/NavItem"
+import { NavButton } from "../NavBar/NavButton"
+import { Icon } from "./Icon";
+import { alphaDown, alphaUp } from "../../img/icons";
 
 
 const ControlsDiv = styled.div`
@@ -16,18 +18,24 @@ const ControlsDiv = styled.div`
 export const Controls = () => {
 
     const dispatch = useDispatch();
-    let   pathname = useLocation().pathname;
+    let pathname = useLocation().pathname;
     // const inputRef = useRef();
 
     return (
         <ControlsDiv>
             {/* <NavItem>  */}
-        <NavButton onClick = {() => dispatch(sortTitlesAscending())}>Sort Title Ascending</NavButton>
+            {/* <NavButton onClick = {() => dispatch(sortTitlesAscending())}>Sort Title Ascending</NavButton> */}
+            <NavButton onClick={() => dispatch(sortTitlesAscending())}>
+                <Icon src={alphaUp} />
+            </NavButton>
             {/* </NavItem> */}
-        <NavButton onClick = {() => dispatch(sortTitlesDescending())}>Sort Title Descending</NavButton>
-        <NavButton onClick = {() => dispatch(sortDatesAscending())}>Sort Date Ascending</NavButton>
-        <NavButton onClick = {() => dispatch(sortDatesDescending())}>Sort Date Descending</NavButton>
-        <NavButton onClick = {() => dispatch(addTask(pathname))}>+</NavButton>
+            {/* <NavButton onClick = {() => dispatch(sortTitlesDescending())}>Sort Title Descending</NavButton> */}
+            <NavButton onClick={() => dispatch(sortTitlesDescending())}>
+                <Icon src={alphaDown} />
+            </NavButton>
+            <NavButton onClick={() => dispatch(sortDatesAscending())}>Sort Date Ascending</NavButton>
+            <NavButton onClick={() => dispatch(sortDatesDescending())}>Sort Date Descending</NavButton>
+            <NavButton onClick={() => dispatch(addTask(pathname))}>+</NavButton>
         </ControlsDiv>
     );
 
--- src/img/icons/index.js
+++ src/img/icons/index.js
@@ -1,5 +1,7 @@
 import githubIcon from './githubIcon.png';
 import linkedinIcon from './linkedinIcon.png';
 import searchIcon from './searchIcon.png';
+import alphaUp from './sortAlphaUp.png';
+import alphaDown from './sortAlphaDown.png';
 
-export {githubIcon, linkedinIcon, searchIcon};
+export {githubIcon, linkedinIcon, searchIcon, alphaUp, alphaDown};
